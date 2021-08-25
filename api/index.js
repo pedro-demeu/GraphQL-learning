8bd3024e6ae287a85e4445069110d85c0293499e
Pedro <pedromiguelnevesdemeu@gmail.com>
2021-08-25 17:57:41 -0300

    refinando metodo resolvers

--- api/index.js
+++ api/index.js
@@ -1,10 +1,23 @@
 const { ApolloServer } = require('apollo-server')
+
 const userSchema = require('./user/schema/user.graphql')
 const userResolvers = require('./user/resolvers/userResolvers')
 
+const usersAPI = require('./user/datasource/user')
+
 const typeDefs = [userSchema]
 const resolvers = [userResolvers]
-const server = new ApolloServer( { typeDefs, resolvers} )
+
+const server = new ApolloServer( { 
+  typeDefs, 
+  resolvers,
+  dataSources: () => {
+    return{
+      usersAPI: new usersAPI()
+    }
+  }
+
+} )
 
 server.listen().then(({url}) => {
   console.log("Servidor rodando")
